88ebd748dc8c4f59c1b686b8ff1ab24598801dee
bansii18 <bansi84451@gmail.com>
2025-06-12 12:42:18 +0530

    POST DEFINE

--- JSoops/backend/index.js
+++ JSoops/backend/index.js
@@ -2,15 +2,18 @@ const express = require("express");
 const QueryString = require("qs");
 const app = express();
 const port = 8080;
+app.use(express.urlencoded({extended : true}));//for parsing data , so node
+app.use(express.json());// understand different types of data 
 app.listen(port,()=>{
     console.log(`server is litening on port ${port}`);
 });
 app.get("/register",(req,res)=>{
-    let {user, passoword} = req.query;
+    let {user, password} = req.query;
     console.log(user, passoword)
     res.send(`GET Standard Response by ${user}`);
 });
 app.post("/register",(req,res)=>{
-    console.log(req.body);
-    res.send("POST Standard Response");
+    let {user, password} = req.body;
+    //  console.log(req.body);
+    res.send(`POST Standard Response by ${user}`);
 });
